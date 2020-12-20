89885f80f9a120be2b4a27e9524e2fc86f43854f
2018119131_EmbeddedSystem <50951732+li-shaoyu@users.noreply.github.com>
2020-12-20 22:54:31 +0800

    Add files via upload

--- init.c
+++ init.c
@@ -2,8 +2,9 @@
 #include "includeAll.h"
 //=============================================================================
 void SysInit() {
+ F_clearWDT();  //启动后立即清除看门狗
   //时钟初始化
-  CLKCON = 0x03; // Clock div 1
+ CLKCON = 0x03; // Clock div 1// Clock div 1
   STPPCK = 0;
   STPFCK = 0;
   _nop_();
@@ -22,11 +23,15 @@ void SysInit() {
   _nop_();
   _nop_();
   _nop_();
-  // IO 模式初始化
-  P1MODL = 0xa8;
-  P1_0 = 1;
+  
+
+  // IO 模式初始化为推挽输出
+ P1MODL = 0xaa;       
   // IO 状态初始化
-  F_ledOff();
+  F_led1Off();      //LED灯初始化为低电平状态（不亮）
+  F_led2Off();
+  F_led3Off();
+  
 }
 //=============================================================================
-void VarsInit() {}
+void VarsInit() {} 
